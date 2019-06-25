02eda284e7b0b0b5399a506e4ccbb916f8ee1eb3
intgrity-rd-auto <subh078@yahoo.in>
2019-06-25 11:01:59 +0530

    First Maven Project Commit

---
+++ com.EclipseWithGit/src/test/java/com/EclipseWithGit/com/EclipseWithGit/AppTest.java
@@ -0,0 +1,20 @@
+package com.EclipseWithGit.com.EclipseWithGit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
